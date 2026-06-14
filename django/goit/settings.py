b27c048e00f98e31440a14b55ae2229e01476e22
Maksym Kryvenko <maks1997ym@gmail.com>
2026-06-14 22:52:20 +0200

    Updated env vars and allowed hosts

--- django/goit/settings.py
+++ django/goit/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-n*0a^$*(up4zcqe(l(3af0kaiuc!(l(-egt@)*72oih7nu$cy0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'django', 'django_app']
 
 
 # Application definition
@@ -121,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
