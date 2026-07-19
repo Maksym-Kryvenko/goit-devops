c0024621741143445770eb3a9a10289873042c65
Maksym Kryvenko <maks1997ym@gmail.com>
2026-07-19 22:02:21 +0200

    Add main page for django

--- django/goit/urls.py
+++ django/goit/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse("Hello from django-app — version 1 🚀")
+
 
 urlpatterns = [
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
 ]
